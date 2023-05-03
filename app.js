4343a47f26176f4b7d0182f8477caabe4638387c
ErosFlugel <erosflugel18@gmail.com>
2023-05-03 17:27:22 -0500

    Inital Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Requesting Libraries
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Rising up the server
+app.listen(3030, console.log('Servidor corriendo en puerto 3030'));
+
+//Assigning Public Folder
+app.use(express.static(path.resolve(__dirname, './public')));
+
+//Assigning Views
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
