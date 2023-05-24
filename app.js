fa866fd2cbe623ffd43348ee29642954c4236795
ErosFlugel <erosflugel18@gmail.com>
2023-05-24 17:56:35 -0500

    app.js & package.json primer set-up para montar el server a Render

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//Rising up the server
-app.listen(3030, console.log('Servidor corriendo en puerto 3030'));
+//Setting up the local server
+// app.listen(3030, console.log('Servidor corriendo en puerto 3030'));
+
+// Setting up the cloud server for Render
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 //Assigning Public Folder
 app.use(express.static(path.resolve(__dirname, './public')));
