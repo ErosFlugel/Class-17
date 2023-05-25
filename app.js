d6c794ff7ca4c11911b675c5497dba162499bdb8
ErosFlugel <erosflugel18@gmail.com>
2023-05-24 20:51:10 -0500

    tenia el css en minuscula para el path cuando la carpeta estaba en mayusculas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 
