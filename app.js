1472fc75e3a4c0f6fc1ddfa0a0a49083bf59ab31
ErosFlugel <erosflugel18@gmail.com>
2023-05-24 20:32:34 -0500

    borrando los comentarios de ayuda en el app.js para que funcione el Render

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//Setting up the local server
-// app.listen(3030, console.log('Servidor corriendo en puerto 3030'));
-
-// Setting up the cloud server for Render
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
-//Assigning Public Folder
+
 app.use(express.static(path.resolve(__dirname, './public')));
 
-//Assigning Views
+
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/registro', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
